7683837c03a53e5164b7138e7a1d786d6579a076
SlavaKuntsevich <mactep_chif@mail.ru>
2023-01-31 23:46:42 +0300

    1.1

--- Program.cs
+++ Program.cs
@@ -8,7 +8,10 @@
 
 
             Console.Write("Hello, World!");
+
             Console.Write("Hello, World!");
+            Console.Write("Hello, World!");
+            Console.Write("Good bye, World!");
 
         }
     }
